3dbcb849f0125fbdb0ab3185bf0ffb0745da14f5
SANTIAGO SALAS GRANJA <santiago.salas@uao.edu.co>
2024-09-12 19:06:29 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//ternarios
+let edad=12;
+let mensaje = edad>10? "eres un puberto":"eres un adolescente";
+document.write(mensaje);
+
+//intervals
+const saludo = ()=>{
+    document.write("<li>hola UAO</li>")
+};
+
+setInterval(saludo,3000)
